0d7e51bc773b84141cc8ea42bf38fb263dcac931
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-11-01 17:27:09 +0300

    add save file in txt/bin extension

--- src/MyForm.cpp
+++ src/MyForm.cpp
@@ -8,6 +8,7 @@
 
 using namespace coursework;
 using namespace msclr::interop;
+using namespace System::IO;
 
 [System::STAThread]
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -95,8 +96,20 @@ System::Void coursework::MyForm::saveToolStripMenuItem_Click(System::Object^ sen
 	saveFileDialog1->ShowDialog();
 	try
 	{
-		String^ filepath = saveFileDialog1->FileName;
-		System::IO::File::WriteAllText(filepath, Output->Text);
+		std::string filepath = marshal_as<std::string>(saveFileDialog1->FileName);
+		std::string extenstion = filepath;
+		if (extenstion.erase(0, extenstion.rfind(".") + 1) == "txt") {
+			std::ofstream fText(filepath);
+			std::string data = marshal_as<std::string>(Output->Text);
+			fText << data;
+			fText.close();
+		}
+		else {
+			std::ofstream fBin(filepath, std::ios::out | std::ios::binary);
+			std::string data = marshal_as<std::string>(Output->Text);
+			fBin.write(reinterpret_cast<char*>(&data), sizeof(data));
+			fBin.close();
+		}
 	}
 	catch (Exception^) {}
 }
@@ -104,12 +117,31 @@ System::Void coursework::MyForm::saveToolStripMenuItem_Click(System::Object^ sen
 System::Void coursework::MyForm::openToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
 	openFileDialog1->ShowDialog();
 	try {
-		String^ filepath = openFileDialog1->FileName;
-		array <String^>^ lines = System::IO::File::ReadAllLines(filepath);
+		std::string filepath = marshal_as<std::string>(openFileDialog1->FileName);
+		std::string extenstion = filepath;
 		Output->Text = "";
-		for (int i = 0; i < lines->Length; i++) {
-			Output->Text += lines[i] + Environment::NewLine;
+		if (extenstion.erase(0, extenstion.rfind(".") + 1) == "txt") {
+			std::ifstream iText(filepath);
+			std::string line;
+			if (iText.is_open())
+			{
+				while (getline(iText, line))
+				{
+					Output->Text += marshal_as<String^>(line) + Environment::NewLine;
+				}
+			}
+			iText.close();
 		}
+		else {
+			std::ifstream iBin(filepath, std::ios::binary);
+			size_t size = 0;
+			iBin.read(reinterpret_cast<char*>(&size), sizeof(size));
+			std::string data;
+			data.resize(size);
+			iBin.read(reinterpret_cast<char*>(&size), data.size());
+			iBin.close();
+			Output->Text = marshal_as<String^>(data);
+		}	
 	}
 	catch (Exception^) {}
 }
